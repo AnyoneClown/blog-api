194807145296ae705b716e60470c9690eabab9f3
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-13 17:48:23 +0200

    register blog urls, add statis load

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from core import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/users/", include("user.urls", namespace="user"))
-]
+    path("api/users/", include("user.urls", namespace="user")),
+    path("api/blog/", include("blog.urls", namespace="blog")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
